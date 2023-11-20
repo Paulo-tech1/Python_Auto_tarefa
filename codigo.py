2e88c710a98411f5dae8c5eb83d0850f9ffb1db8
Paulo Costa <pauloroberto.redes@gmail.com>
2023-11-20 18:47:22 -0300

    Python autocacao
    Python automação de tarefas

---
+++ codigo.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+# abrir o navegador (chrome)
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
